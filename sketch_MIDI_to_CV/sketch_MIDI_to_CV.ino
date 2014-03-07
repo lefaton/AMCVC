c29cb1a3f93c0a065a0caeffc55b1e693f698067
Matthieu <utopikprod@gmail.com>
2014-03-07 02:05:06 -0500

    Add Read/Write function
    Just add some logic in the code, nothing tested yet.

--- sketch_MIDI_to_CV/sketch_MIDI_to_CV.ino
+++ sketch_MIDI_to_CV/sketch_MIDI_to_CV.ino
@@ -7,12 +7,15 @@ const int midiInput = 2;
 const int UARTOut = 3;
 const int ledPin = 6;
 
-const byte midiMessageBuffer[16] = {0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0};
-const byte midiNoteBuffer[16] = {0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0};
-const byte midiVelocityBuffer[16] = {0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0};
+byte midiMessageBuffer[10] = {0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0};
+byte midiNoteBuffer[10] = {0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0};
+byte midiVelocityBuffer[10] = {0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0};
 
-const int bufferPosition = 0;
-const int bufferReadingPosition = 0;
+int bufferPosition = 0;
+
+const byte accentCC = 0x64;
+const byte slideCC = 0x66;
+const byte triggerLimit = 0x64;
 
 //MIDI specific table
 const byte noteOff = 0x80;
@@ -38,12 +41,96 @@ void setup()
  pinMode(ledPin, OUTPUT);
 }
 
+void ReadData()
+{
+  midiMessageBuffer[bufferPosition] = Serial.read();
+   
+  //Midi Off Mess
+  if(midiMessageBuffer[bufferPosition] > noteOff && midiMessageBuffer[bufferPosition] < noteOn)
+  { 
+    midiNoteBuffer[bufferPosition] = Serial.read();
+    midiVelocityBuffer[bufferPosition] = Serial.read();
+  }
+  else
+  {
+    //Midi On Mess
+    if(midiMessageBuffer[bufferPosition] > noteOn && midiMessageBuffer[bufferPosition] < aftertouch)
+    {
+      midiNoteBuffer[bufferPosition] = Serial.read();
+      midiVelocityBuffer[bufferPosition] = Serial.read();
+    }
+    else
+    {
+      //Midi CC Mess
+      if(midiMessageBuffer[bufferPosition] > cc && midiMessageBuffer[bufferPosition] < patchRange)
+      {
+        midiNoteBuffer[bufferPosition] = Serial.read();
+        midiVelocityBuffer[bufferPosition] = Serial.read();
+      }
+    }
+  }
+}
+
+void ProcessData()
+{  
+  //Midi Off Mess
+  if(midiMessageBuffer[bufferPosition] > noteOff && midiMessageBuffer[bufferPosition] < noteOn)
+  { 
+    //Set gate voltage to 0V
+  }
+  else
+  {
+    //Midi On Mess
+    if(midiMessageBuffer[bufferPosition] > noteOn && midiMessageBuffer[bufferPosition] < aftertouch)
+    {
+      //Set gate voltage to 5V
+      //Set vco voltage following hexa value in midiNoteBuffer[bufferPosition]
+    }
+    else
+    {
+      //Midi CC Mess
+      if(midiMessageBuffer[bufferPosition] > noteOn && midiMessageBuffer[bufferPosition] < aftertouch)
+      {
+        if(midiNoteBuffer[bufferPosition]==accentCC && midiVelocityBuffer[bufferPosition]>triggerLimit)
+        {
+           //Set On digital out for accent
+        }
+        
+        if(midiNoteBuffer[bufferPosition]==slideCC && midiVelocityBuffer[bufferPosition]>triggerLimit)
+        {
+           //Set On digital out for slide
+        }
+      }
+    }
+  }
+  
+  midiMessageBuffer[bufferPosition] = 0x0;
+  midiNoteBuffer[bufferPosition] = 0x0;
+  midiVelocityBuffer[bufferPosition] = 0x0;
+}
+
+void blinkLED()
+{
+   digitalWrite(ledPin,HIGH);
+   delay(100);
+   digitalWrite(ledPin,LOW);
+   delay(100);
+}
+
 void loop()
 {
- if(Serial.available())
- {
-   MIDI.read();
- } 
- 
+  
+  while(Serial.available() && bufferPosition<10)
+  {
+    ReadData();
+    bufferPosition = ++bufferPosition%10;
+    blinkLED();
+  }
+  
+  while(bufferPosition>0)
+  {
+    ProcessData();
+    --bufferPosition;
+  }
   
 }
