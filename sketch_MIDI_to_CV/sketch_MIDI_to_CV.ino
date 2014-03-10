f2701ffff77045465f24e304c9ddd24e02b0c5f9
Matthieu <utopikprod@gmail.com>
2014-03-10 00:53:31 -0400

    Add DAC doc and code
    Continue with untested code. I will have to test many electronic things
    concerning timing issues before to continue.

--- sketch_MIDI_to_CV/sketch_MIDI_to_CV.ino
+++ sketch_MIDI_to_CV/sketch_MIDI_to_CV.ino
@@ -2,11 +2,25 @@
 Get MIDI message and transform them into Hexa numbers driving a 12 bits DAC
 */
 
-//const variables
-const int midiInput = 2;
-const int UARTOut = 3;
+//MIDI channel used
+const int MIDIChannel = 3;
+
+//const IO variables
+const int midiInputPin = 2;
+const int DACInputPin = 3;
 const int ledPin = 6;
 
+//MAX526 DAC control
+const int CSMSBPin = 4; //CSLSB true by default using a 74HC04 //they are always inverted
+const int WRPin = 5; //To trigger after write
+const int LDACPin = 7; //Load written bytes into DAC registers
+const int Add0Pin = 8;
+const int Add1Pin = 9;
+
+byte LSBytes = 0x0;
+byte MSBytes = 0x0;
+
+//buffer receiver
 const int bufferSize = 10;
 
 byte midiMessageBuffer[bufferSize] = {0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0};
@@ -15,6 +29,7 @@ byte midiVelocityBuffer[bufferSize] = {0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0};
 
 int bufferPosition = 0;
 
+//x0x heart specific control
 const byte accentCC = 0x64;
 const byte slideCC = 0x66;
 const byte triggerLimit = 0x64;
@@ -38,19 +53,97 @@ const byte stopSeq = 0xFC;
 void setup()
 {
  Serial.begin(31250);
- pinMode(midiInput, INPUT);
- pinMode(UARTOut, OUTPUT);
+ pinMode(midiInputPin, INPUT);
+ pinMode(DACInputPin, OUTPUT);
  pinMode(ledPin, OUTPUT);
+ pinMode(CSMSBPin, OUTPUT);
+ pinMode(WRPin, OUTPUT);
+ pinMode(LDACPin, OUTPUT);
+ pinMode(Add0Pin, OUTPUT);
+ pinMode(Add1Pin, OUTPUT);
+}
+
+void WriteBytes(int DACOutput)
+{
+  DACOutputToWriteIn(DACOutput);
+  
+  //LSB first
+  digitalWrite(CSMSBPin,HIGH);//inverted (LOW set active)
+  digitalWrite(WRPin,LOW);
+  
+  Serial.print(LSBytes,BYTE);
+  digitalWrite(WRPin,HIGH);
+   
+  //MSB Second
+  digitalWrite(CSMSBPin,LOW);
+  digitalWrite(WRPin,LOW);
+  
+  Serial.print(MSBytes,BYTE);
+  digitalWrite(WRPin,HIGH);
+  
+  //actually write into DAC register
+  digitalWrite(LDACPin,LOW);
+  digitalWrite(LDACPin,HIGH);
+}
+  
+void DACOutputToWriteIn(int input)
+{
+  if(input == 0)
+  {
+    digitalWrite(Add0Pin,LOW);
+    digitalWrite(Add1Pin,LOW);
+  }  
+  else
+  {
+    if(input == 1)
+    {
+      digitalWrite(Add0Pin,HIGH);
+      digitalWrite(Add1Pin,LOW);
+    }
+    else
+    {
+      if(input == 2)
+      {
+        digitalWrite(Add0Pin,LOW);
+        digitalWrite(Add1Pin,HIGH);
+      }
+      else
+      {
+        digitalWrite(Add0Pin,HIGH);
+        digitalWrite(Add1Pin,HIGH); 
+      }
+    }
+  }
 }
 
 void ReadData()
 {
   midiMessageBuffer[bufferPosition] = Serial.read();
+  
+  if( midiMessageBuffer[bufferPosition] & 0x0F != MIDIChannel)
+    {
+      midiMessageBuffer[bufferPosition] = 0x0;
+      midiNoteBuffer[bufferPosition] = 0x0;
+      midiVelocityBuffer[bufferPosition] = 0x0;
+    }
+    
    
   if(midiMessageBuffer[bufferPosition] > noteOff && midiMessageBuffer[bufferPosition] < patchRange)
-  { 
-    midiNoteBuffer[bufferPosition] = Serial.read();
-    midiVelocityBuffer[bufferPosition] = Serial.read();
+  {
+    if( midiMessageBuffer[bufferPosition] & 0x0F != MIDIChannel)
+    {
+      //ignore other channels, read in nowhere
+      Serial.read();
+      Serial.read();
+    } 
+    else
+    {
+      midiNoteBuffer[bufferPosition] = Serial.read();
+      midiVelocityBuffer[bufferPosition] = Serial.read();
+    
+      ++bufferPosition;
+      blinkLED();
+    }
   }
   else
   {
@@ -89,11 +182,15 @@ void ProcessData()
         }
       }
     }
+  
+    
   }
   
   midiMessageBuffer[bufferPosition] = 0x0;
   midiNoteBuffer[bufferPosition] = 0x0;
   midiVelocityBuffer[bufferPosition] = 0x0;
+  
+  --bufferPosition;
 }
 
 void blinkLED()
@@ -110,14 +207,11 @@ void loop()
   while(Serial.available() && bufferPosition<bufferSize)
   {
     ReadData();
-    bufferPosition = ++bufferPosition;
-    blinkLED();
   }
   
   while(bufferPosition>0)
   {
-    ProcessData();
-    --bufferPosition;
+    ProcessData(); 
   }
   
 }
